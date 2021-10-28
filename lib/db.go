b37221bf4d2d14daa96bdb57588363836cc493d7
mahaoyuan <1440970573@qq.com>
2021-10-28 18:17:58 +0800

    initial database conn

--- lib/db.go
+++ lib/db.go
@@ -11,7 +11,7 @@ var db *gorm.DB
 var err error
 
 func InitDb() {
-	db, err = gorm.Open("mysql", "root:root@/blog?charset=utf8&parseTime=True&loc=Local")
+	db, err = gorm.Open("mysql", "root:bhjx@309@tcp(10.20.13.33:3306)/software_theory?charset=utf8&parseTime=True&loc=Local")
 	if err != nil {
 		fmt.Println("连接数据库失败，请检查参数：", err)
 		os.Exit(1)
--- main.go
+++ main.go
@@ -1,12 +1,14 @@
 package main
 
 import (
+	"server/lib"
+	"server/models"
 	"server/router"
 )
 
 func main() {
-	//lib.InitDb()
-	//_ = lib.GetDBConn().AutoMigrate(&models.User{})
+	lib.InitDb()
+	_ = lib.GetDBConn().AutoMigrate(&models.User{})
 
 	router.InitRouter()
 
